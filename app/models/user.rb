f663a4481602782f72ac745fcf39e50f96b465e0
Tatsuya Oba <toba17893@gmail.com>
2019-06-02 02:09:52 +0900

    login with google

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  devise :omniauthable, omniauth_providers: %i[google_oauth2]
+
+  class << self
+    def from_omniauth(auth)
+      user = User.find_by(uid: auth.uid, provider: auth.provider)
+      if user.nil?
+        user = User.create!(
+            uid: auth.uid,
+            provider: auth.provider,
+            email: auth.info.email,
+            password: Devise.friendly_token[0, 20]
+        )
+      end
+      user
+    end
+  end
+end
