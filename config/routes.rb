83d1f9774771991cfad5b786723c0af7eda03c2f
Tatsuya Oba <toba17893@gmail.com>
2019-06-11 00:30:50 +0900

    set redirect when noauthenticate

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  root 'home#index'
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  unauthenticated :user do
+    root 'home#index'
+  end
+
   authenticated :user do
+    root 'word_cards#index'
     resources :word_cards do
       collection do
         get :study
       end
       resources :word_card_results, only: [:create]
     end
-
   end
 end
