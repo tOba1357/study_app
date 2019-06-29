c5bf9c2851f75700f82a56d44d997cb3b07372ef
Tatsuya Oba <toba17893@gmail.com>
2019-06-29 16:47:51 +0900

    サマリーする範囲を限定する

--- app/models/word_card_result.rb
+++ app/models/word_card_result.rb
@@ -15,13 +15,43 @@
 
 class WordCardResult < ApplicationRecord
   belongs_to :word_card
-  after_save -> do
-    summary = WordCardResultSummary.find_or_initialize_by(word_card_id: word_card_id)
-    summary.result_count = WordCardResult.where(word_card_id: word_card_id).count
-    summary.remember_count = WordCardResult
-                                 .where(word_card_id: word_card_id)
-                                 .where(remember: true)
-                                 .count
-    summary.save!
-  end
+  after_commit -> do
+    begin
+      ActiveRecord::Base.transaction do
+        user = word_card.user
+        conn = ActiveRecord::Base.connection
+        sql = <<-sql
+    delete from word_card_result_summaries 
+    where word_card_id in (select id from word_cards where user_id = ? and deleted_at is null)
+        sql
+        sql = ActiveRecord::Base.send(
+            :sanitize_sql_array,
+            [sql, user.id]
+        )
+        conn.execute(sql)
+
+        sql = <<-sql
+insert into word_card_result_summaries (word_card_id, result_count, remember_count, created_at, updated_at)
+select 
+  word_card_id,
+  sum(case when remember then 1 else 0 end) remember_count,
+  count(*) result_count,
+  current_timestamp,
+  current_timestamp
+from (
+  select *
+  from word_card_results
+  where word_card_id in (select id from word_cards where user_id = ? and deleted_at is null)
+  order by created_at desc
+  limit ?) sliced
+group by word_card_id;
+        sql
+        sql = ActiveRecord::Base.send(
+            :sanitize_sql_array,
+            [sql, user.id, user.word_cards.count * 2]
+        )
+        conn.execute(sql)
+      end
+    end
+ end
 end
